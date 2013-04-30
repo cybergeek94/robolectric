afde7a11cf511a4c1603058a1c1d5ef02f5f5876
Christian Williams <xian@squareup.com>
2013-04-30 16:29:41 -0700

    WIP

--- src/main/java/org/robolectric/res/Attribute.java
+++ src/main/java/org/robolectric/res/Attribute.java
@@ -60,6 +60,10 @@ public class Attribute {
         return matcher.group(1);
     }
 
+    public ResName getReferenceResName() {
+        return ResName.qualifyResName(value.substring(1).replace("+", ""), contextPackageName, null);
+    }
+
     @Override
     public String toString() {
         return "Attribute{" +
@@ -133,10 +137,18 @@ public class Attribute {
     }
 
     public String qualifiedValue() {
-        if (value.startsWith("@")) {
+        if (isReference()) {
             return ResName.qualifyResourceName(value.substring(1), contextPackageName, null);
         } else {
             return value;
         }
+    }
+
+    public boolean isReference() {
+        return value.startsWith("@");
+    }
+
+    public boolean isNull() {
+        return "@null".matches(value);
     }
 }
